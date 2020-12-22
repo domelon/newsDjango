c52fa43f13f9d03ef114e4a3d21a978f10c36c97
Mariusz Szymanski <mariusz@melonio.pl>
2020-12-22 11:30:32 +0100

    edit .gitignore

---
+++ articles/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.urls import reverse
+
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    slug = models.SlugField(null=False, unique=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('article_detail', kwargs={'slug': self.slug})
---
+++ articles/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import ListView, DetailView
+from .models import Article
+
+
+# Create your views here.
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'article_list.html'
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'article_detail.html'
